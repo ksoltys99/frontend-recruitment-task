5c3b1d9c079e054095b3e98ae176dd50ad596f16
ksoltys99 <86491612+ksoltys99@users.noreply.github.com>
2022-06-09 05:50:24 +0200

    Add files via upload
    All functionalities work properly.

--- src/js/main.js
+++ src/js/main.js
@@ -4,9 +4,10 @@ const modal = document.getElementById('modal');
 const openModal = () => {
     modal.showModal();
 
-    document.querySelector('.close-modal-btn').addEventListener('click', ()=>{
+    document.querySelector('.close-modal-btn').addEventListener('click', () => {
         modal.close();
     })
+
 }
 
 const buttonClickCounter = () => {
@@ -15,11 +16,35 @@ const buttonClickCounter = () => {
 
     if(!isNaN(getCounter)) counter = getCounter;
     localStorage.setItem('buttonClickCounter', ++counter);
+
+    const currentValue = parseInt(counter) + 1;
+
+    updateCounterText(currentValue);   
     
-    console.log(counter);
+    if(currentValue > 5) createClearButton();
+}
+
+const createClearButton = () => {
+    const newButton = document.createElement('button');
+    newButton.innerText = 'Clear';
+
+    document.querySelector('.dialog-box').append(newButton);
+
+    newButton.addEventListener('click', () => {
+        localStorage.clear();
+        updateCounterText(0);
+    })
+}
+
+const updateCounterText = (value) => {
+    document.querySelector('.click-counter-info').innerHTML = `You have clicked ${value} times to related button`;
 }
 
 button.addEventListener('click', () => {
     openModal();
     buttonClickCounter();
 });
+
+document.addEventListener('click', (ev) => {
+    if(ev.target.outerHTML.startsWith('<dialog')) modal.close();
+})
