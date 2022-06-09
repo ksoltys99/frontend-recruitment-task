ad44452f760c63c8799cc7e2a633aae581fe43aa
ksoltys99 <86491612+ksoltys99@users.noreply.github.com>
2022-06-09 06:44:20 +0200

    Add files via upload
    Fixed counter bug, clear button is now hidden

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,49 @@
-const button = document.querySelector('.info-container-button');
+const showPopupBtn = document.querySelector('.info-container-button');
 const modal = document.getElementById('modal');
+const clearBtn = document.querySelector('.clear-button');
 
 const openModal = () => {
     modal.showModal();
 
-    document.querySelector('.close-modal-btn').addEventListener('click', () => {
+    document.querySelector('.close-modal').addEventListener('click', () => {
         modal.close();
     })
-
 }
 
 const buttonClickCounter = () => {
     let counter = 0;
     const getCounter = localStorage.getItem('buttonClickCounter');
 
-    if(!isNaN(getCounter)) counter = getCounter;
+    if(!isNaN(getCounter) && getCounter!==null) counter = parseInt(getCounter);
     localStorage.setItem('buttonClickCounter', ++counter);
 
-    const currentValue = parseInt(counter) + 1;
+    updateCounterText(counter);   
 
-    updateCounterText(currentValue);   
-    
-    if(currentValue > 5) createClearButton();
+    if(counter > 5) showClearButton();
 }
 
-const createClearButton = () => {
-    const newButton = document.createElement('button');
-    newButton.innerText = 'Clear';
-
-    document.querySelector('.dialog-box').append(newButton);
-
-    newButton.addEventListener('click', () => {
-        localStorage.clear();
-        updateCounterText(0);
-    })
+const showClearButton = () => {
+    clearBtn.classList.remove('invisible');
 }
 
 const updateCounterText = (value) => {
-    document.querySelector('.click-counter-info').innerHTML = `You have clicked ${value} times to related button`;
+    document.querySelector('.click-counter-info').innerHTML = `You have clicked <b>${value} ${value === 1 ? `time` : `times`}</b> to related button`;
 }
 
-button.addEventListener('click', () => {
+showPopupBtn.addEventListener('click', () => {
     openModal();
     buttonClickCounter();
 });
 
+clearBtn.addEventListener('click', () => {
+    localStorage.clear();
+    updateCounterText(0);
+    clearBtn.classList.add('invisible');
+})
+
 document.addEventListener('click', (ev) => {
-    if(ev.target.outerHTML.startsWith('<dialog')) modal.close();
+    if(ev.target.outerHTML.startsWith('<dialog')) {
+        modal.close();
+        clearBtn.classList.add('invisible');
+    }
 })
